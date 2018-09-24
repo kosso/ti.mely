c24b1f8b27cd6e3065615472345844a9f6746166
Kosso <kosso1@gmail.com>
2018-09-24 14:02:31 +0100

    Updated file structures and names.

---
+++ ios/build/timely.build/Release-iphoneos/timely.build/Objects-normal/arm64/TiMelyModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: /Users/kosso/_GITHUB/ti.mely/ios/TiMely_Prefix.pch \
+  /Users/kosso/_GITHUB/ti.mely/ios/Classes/TiMelyModuleAssets.m \
+  /Users/kosso/_GITHUB/ti.mely/ios/Classes/TiMelyModuleAssets.h
---
+++ ios/build/timely.build/Release-iphonesimulator/timely.build/Objects-normal/i386/MSWeakTimer.d
@@ -0,0 +1,3 @@
+dependencies: /Users/kosso/_GITHUB/ti.mely/ios/TiMely_Prefix.pch \
+  /Users/kosso/_GITHUB/ti.mely/ios/Classes/MSWeakTimer.m \
+  /Users/kosso/_GITHUB/ti.mely/ios/Classes/MSWeakTimer.h
